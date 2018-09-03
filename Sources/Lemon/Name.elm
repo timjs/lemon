53d605d40c141113f02f7525d765bc9f394297d8
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2018-09-03 15:39:18 +0200

    define abstract syntax

---
+++ Sources/Lemon/Name.elm
@@ -0,0 +1,18 @@
+module Lemon.Name exposing (Name, isLower, isUpper)
+
+
+type alias Name =
+  String
+
+
+checkFirst : (Char -> Bool) -> String -> Bool
+checkFirst pred =
+  String.uncons >> Maybe.map (Tuple.first >> pred) >> Maybe.withDefault False
+
+
+isUpper : Name -> Bool
+isUpper = checkFirst Char.isUpper
+
+
+isLower : Name -> Bool
+isLower = checkFirst Char.isLower
---
+++ Sources/Lemon/Syntax.elm
@@ -0,0 +1,96 @@
+module Lemon.Syntax exposing (Module(..))
+
+import Dict exposing (Dict)
+import Lemon.Name exposing (..)
+
+
+
+-- Modules and Definitions -----------------------------------------------------
+
+
+type Module
+  = Module (List Declaration)
+
+
+type Declaration
+  = Value Name Type (List Pattern) Expression
+
+
+
+-- Expressions -----------------------------------------------------------------
+
+
+type Expression
+  = Atom Atom
+  | Lambda ( Pattern, Type ) Expression
+  | Call Expression Expression
+  | Let (List Declaration) Expression
+  | Case Expression (List ( Pattern, Expression ))
+  | If Expression Expression Expression
+  | Sequence (List Statement)
+
+
+type Statement
+  = Set Pattern Expression
+  | Bind Pattern Expression
+  | Do Expression
+  | Par (List (List Statement))
+  | On (List ( Name, Expression, List Statement ))
+  | When (List ( Expression, List Statement ))
+  | Done
+
+
+
+-- Atoms -----------------------------------------------------------------------
+
+
+type Atom
+  = Basic Basic
+  | Variable Name
+  | Some Expression
+  | None
+  | List (List Expression)
+  | Record (Dict Name Expression)
+
+
+type Basic
+  = Bool Bool
+  | Int Int
+  | Float Float
+  | String String
+
+
+
+-- Patterns --------------------------------------------------------------------
+
+
+type Pattern
+  = PBasic Basic
+  | PVariable Name
+  | PSome Pattern
+  | PNone
+  | PCons Pattern Pattern
+  | PNil
+  | PRecord (Dict Name Pattern)
+  | PIgnore
+
+
+
+-- Types -----------------------------------------------------------------------
+
+
+type Type
+  = TBasic BasicType
+  | TVariable Name
+  | TOption Type
+  | TList Type
+  | TRecord (Dict Name Type)
+  | TTask Type
+  | TArrow Type Type
+
+
+type BasicType
+  = TBool
+  | TInt
+  | TFloat
+  | TString
