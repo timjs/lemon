91fefd124581eae193b36a5ee91c59439adef8dc
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2018-09-10 14:45:10 +0200

    remove legacy modules

--- src/Helpers.purs
+++
@@ -1,7 +0,0 @@
-module Helpers exposing (combine)
-
--- Results ---------------------------------------------------------------------
-
-
-combine : List (Result x a) -> Result x (List a)
-combine = List.foldr (Result.map2 (::)) (Ok [])
