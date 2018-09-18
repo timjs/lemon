aba502dc98830df07f93d8a68e925b6c523a0b21
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2018-09-18 16:28:02 +0200

    update Result helpers

--- Sources/Helpers/Result.elm
+++ Sources/Helpers/Result.elm
@@ -9,75 +9,112 @@ module Helpers.Result exposing
   , join
   )
 
-import Basics.Extra exposing (swap)
+import Basics.Extra exposing (flip)
 import Result exposing (..)
 import Result.Extra exposing (..)
 
 
 
--- Traversable on  Lists -------------------------------------------------------
+-- On Lists --------------------------------------------------------------------
 
 
-{-| Actually `traverse` on Lists and Results
+{-| Map a function producing results on a list
+and combine those into a single result (holding a list).
+Also known as `traverse` on lists.
+
+    combineMap f xs == combine (List.map f xs)
+
 -}
 combineMap : (a -> Result x b) -> List a -> Result x (List b)
-combineMap g xs =
-  combine (List.map g xs)
+combineMap f =
+  combine << List.map f
 
 
-
--- Traversable on Tuples -------------------------------------------------------
-
-
-{-| Actually `sequence` on Tuples and Results
+{-| Pull a result out of the _first_ element of a tuple
+and combine it into a result holding the tuple's values.
 -}
 combineFirst : ( Result x a, c ) -> Result x ( a, c )
-combineFirst = map swap << combineSecond << swap
+combineFirst ( rx, y ) =
+  Result.map (flip Tuple.pair y) rx
 
 
-{-| Actually `sequence` on Tuples and Results
+
+-- On Tuples -------------------------------------------------------------------
+
+
+{-| Pull a result out of the _second_ element of a tuple
+and combine it into a result holding the tuple's values.
+Also known as `sequence` on tuples.
 -}
 combineSecond : ( c, Result x a ) -> Result x ( c, a )
 combineSecond ( x, ry ) =
-  map (Tuple.pair x) ry
+  Result.map (Tuple.pair x) ry
 
 
+{-| Combine all results in a tuple
+into a single result holding the tuple's values.
+Also know as `bisequence` on tuples.
+-}
 combineBoth : ( Result x a, Result x b ) -> Result x ( a, b )
 combineBoth ( rx, ry ) =
-  map2 Tuple.pair rx ry
+  Result.map2 Tuple.pair rx ry
 
 
+{-| Map a function producing results on the _first_ element of a tuple
+and then pull it out using `combineFirst`.
+Also know as `sequence` on tuples.
+
+    combineMapFirst f ( x, y )
+      == combineFirst (Tuple.mapFirst f ( x, y ))
+      == Result.map (flip Tuple.pair y) (f x)
+
+-}
 combineMapFirst : (a -> Result x b) -> ( a, c ) -> Result x ( b, c )
-combineMapFirst f xy =
-  combineFirst (Tuple.mapFirst f xy)
+combineMapFirst f =
+  combineFirst << Tuple.mapFirst f
 
 
-{-| Actually `traverse` on Tuples and Results
+{-| Map a function producing results on the _second_ element of a tuple
+and then pull it out using `combineSecond`.
+Also know as `traverse` on tuples.
 
-    combineMapSecond f ( x, y ) =
-      map (Tuple.pair x) (f y)
+    combineMapSecond f ( x, y )
+      == combineSecond (Tuple.mapSecond f ( x, y ))
+      == Result.map (Tuple.pair x) (f y)
 
 -}
 combineMapSecond : (a -> Result x b) -> ( c, a ) -> Result x ( c, b )
-combineMapSecond f xy =
-  combineSecond (Tuple.mapSecond f xy)
+combineMapSecond f =
+  combineSecond << Tuple.mapSecond f
 
 
-{-| Actually `bitraverse` on Tuples and Results
+{-| Map a function producing results on the _both_ elements of a tuple
+and then pull them out using `combineBoth`.
+Also know as `bitraverse` on tuples.
 
-    combineMapBoth f g ( x, y ) =
-      map2 Tuple.pair (f x) (g y)
+    combineMapBoth f g ( x, y )
+      == combineBoth (Tuple.mapBoth f g ( x, y ))
+      == Result.map2 Tuple.pair (f x) (g y)
 
 -}
 combineMapBoth : (a -> Result x c) -> (b -> Result x d) -> ( a, b ) -> Result x ( c, d )
-combineMapBoth f g xy =
-  combineBoth (Tuple.mapBoth f g xy)
+combineMapBoth f g =
+  combineBoth << Tuple.mapBoth f g
 
 
 
 -- Monad -----------------------------------------------------------------------
 
 
+{-| Join contained results with the same error into one result.
+
+Usefull if you have a "result in a result":
+
+    join <| Ok (Ok 4) == Ok 4
+
+    join <| Ok (Err "message") == Err "message"
+
+-}
 join : Result x (Result x a) -> Result x a
 join r =
   case r of
