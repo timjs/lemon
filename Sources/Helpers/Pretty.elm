1c00af30d8303b0a75d29a24bec0b8d38e56cbc7
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2018-09-18 16:59:11 +0200

    add documentation to helpers

--- Sources/Helpers/Pretty.elm
+++ Sources/Helpers/Pretty.elm
@@ -4,26 +4,74 @@ import Basics.Extra exposing (flip)
 import Pretty exposing (..)
 
 
+{-| Short hand notation for append.
+Usefull when appending multiple parts together:
+
+    string "Hello"
+      |> a space
+      |> a "World"
+      |> a (char '!')
+      |> a line
+
+-}
 a : Doc -> Doc -> Doc
 a = flip append
 
 
+{-| Concatenate a list of documents together interspersed with lines.
+Very convenient when laying out lines after another:
+
+    lines
+      [ string "Heading"
+      , empty
+      , words [string "First", string "paragraph"]
+      ...
+      ]
+
+    ==
+
+    string "Heading"
+      |> a line
+      |> a line
+      |> a (string "First")
+      |> a space
+      |> a (string "paragraph")
+      ...
+
+See also `words`.
+
+-}
 lines : List Doc -> Doc
 lines = join line
 
 
+{-| Like `lines` but uses `softline` instaed.
+-}
 softlines : List Doc -> Doc
 softlines = join softline
 
 
+{-| Concatenate a list of documents together interspersed with spaces.
+Very convenient when laying out words after another.
+
+See also `lines`.
+
+-}
 words : List Doc -> Doc
 words = join space
 
 
+{-| Fold a list of documents from left to right using a given function.
+
+    fold f == List.foldl f empty
+
+-}
 fold : (a -> Doc -> Doc) -> List a -> Doc
 fold f =
   List.foldl f empty
 
 
+{-| Wraps a document in brackets.
+-}
 brackets : Doc -> Doc
 brackets = surround (char '[') (char ']')
